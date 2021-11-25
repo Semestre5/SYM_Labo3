22a6c14dc787a22ea82eeb98e3170ce7f9c5ce0d
Axel Vallon <axel.vallon123@gmail.com>
2021-11-25 18:09:46 +0100

    init proj

---
+++ labo3/app/src/main/java/ch/heigvd/sym/labo3/MainActivity.kt
@@ -0,0 +1,31 @@
+package ch.heigvd.sym.labo3
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var barcode: Button
+    private lateinit var nfc : Button
+    private lateinit var beacon: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        barcode = findViewById(R.id.button_barcode)
+        beacon = findViewById(R.id.button_beacon)
+        nfc = findViewById(R.id.button_nfc)
+        barcode.setOnClickListener {
+            //val intent = Intent(this@MainActivity, DeferredActivity::class.java)
+            //startActivity(intent)
+        }
+        beacon.setOnClickListener {
+            //val intent = Intent(this@MainActivity, DeferredActivity::class.java)
+            //startActivity(intent)
+        }
+        nfc.setOnClickListener {
+            //val intent = Intent(this@MainActivity, DeferredActivity::class.java)
+            //startActivity(intent)
+        }
+    }
+}
