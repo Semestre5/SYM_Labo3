564779409fc76a7858229076ecbbb92e3658a7fa
Rotho1998 <rob.gaudin@hotmail.com>
2021-12-08 13:55:09 +0100

    Error receiver

--- labo3/app/src/main/java/ch/heigvd/sym/labo3/beacon/Beacon.kt
+++ labo3/app/src/main/java/ch/heigvd/sym/labo3/beacon/Beacon.kt
@@ -1,28 +1,48 @@
 package ch.heigvd.sym.labo3.beacon
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ch.heigvd.sym.labo3.R
 import android.bluetooth.BluetoothAdapter
 import android.content.Intent
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import org.altbeacon.beacon.BeaconManager
+import org.altbeacon.beacon.BeaconParser
 
+private const val BEACON_FORMAT: String = "m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"
+private const val INTERVAL: Int = 1000
 
 class Beacon : AppCompatActivity() {
+
+    private lateinit var beaconManager: BeaconManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_beacon)
 
-        // Chack if Bluetooth is activated, and ask the user to activate it if not
         val REQUEST_ENABLE_BT = 1
         val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
         // Test if the Bluetooth is supported on the device
         if (bluetoothAdapter != null) {
+            // Check if Bluetooth is activated, and ask the user to activate it if not
             if (bluetoothAdapter.isEnabled == false) {
                 val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
                 startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
             }
+        } else {
+            finish()
         }
 
-        
+        // Ask for permission to use localisation if not granted
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
+        }
+
+        // Initialize BeaconManager
+        beaconManager = BeaconManager.getInstanceForApplication(this)
+        beaconManager.beaconParsers.add(BeaconParser().setBeaconLayout(BEACON_FORMAT))
     }
 }
