d62e1d8fcae9feeeb766436ecc738a6dc6dd3430
Rotho1998 <rob.gaudin@hotmail.com>
2021-12-08 23:13:39 +0100

    Creation of the item for the recyclerView

--- labo3/app/src/main/java/ch/heigvd/sym/labo3/beacon/Beacon.kt
+++ labo3/app/src/main/java/ch/heigvd/sym/labo3/beacon/Beacon.kt
@@ -7,13 +7,16 @@ import ch.heigvd.sym.labo3.R
 import android.bluetooth.BluetoothAdapter
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import org.altbeacon.beacon.BeaconConsumer
 import org.altbeacon.beacon.BeaconManager
 import org.altbeacon.beacon.BeaconParser
+import org.altbeacon.beacon.Region
 
 private const val BEACON_FORMAT: String = "m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"
-private const val INTERVAL: Int = 1000
+private const val INTERVAL: Long = 1000
 
 class Beacon : AppCompatActivity() {
 
@@ -28,9 +31,9 @@ class Beacon : AppCompatActivity() {
         // Test if the Bluetooth is supported on the device
         if (bluetoothAdapter != null) {
             // Check if Bluetooth is activated, and ask the user to activate it if not
-            if (bluetoothAdapter.isEnabled == false) {
-                val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+            if (!bluetoothAdapter.isEnabled) {
+                val toast = Toast.makeText(applicationContext, "Please enable the Bluetooth", Toast.LENGTH_LONG)
+                toast.show()
             }
         } else {
             finish()
@@ -44,5 +47,9 @@ class Beacon : AppCompatActivity() {
         // Initialize BeaconManager
         beaconManager = BeaconManager.getInstanceForApplication(this)
         beaconManager.beaconParsers.add(BeaconParser().setBeaconLayout(BEACON_FORMAT))
+
+        // Set scan interval
+        beaconManager.foregroundBetweenScanPeriod = INTERVAL
+        beaconManager.updateScanPeriods()
     }
 }
